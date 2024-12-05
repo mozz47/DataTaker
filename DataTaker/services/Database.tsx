33337f6e291ba89701a864552285588b9683dabc
Christian Andreas Voß <vosschri@uni-marburg.de>
2024-12-05 14:30:57 +0100

    implemented CRUD db DAO methods

--- DataTaker/services/Database.tsx
+++ DataTaker/services/Database.tsx
@@ -29,7 +29,52 @@ export const DataBase = {
 PRAGMA journal_mode = WAL; --Write-Ahead Logging for perfomance boost
 CREATE TABLE IF NOT EXISTS ${tableName} (${columns});
             `)
+    },
+
+    /** reading data from table tableName
+     * 
+     * @param tableName - string name of the table (that is also by default the database name)
+     * @returns - table in object form
+    */
+    query: async (tableName: string) => {
+        //TODO: check if db and table exist
+        const db = await SQLite.openDatabaseAsync(`${tableName}.db`); // open db
+        //TODO: read table with sql query
+        //TODO: convert to object and return
+        //temporary test query:
+        const allRows: object[] = await db.getAllAsync(`SELECT * FROM ${tableName};`);
+        for (const row of allRows) { // iterate through all rows
+            for (const [key, value] of Object.entries(row)) {  // iterate through all columns
+                console.log(`${key}: ${value}`);  // test console output
+            }
+        }
+    },
+
+    /** adding data to a given table
+     * 
+     * @param tableName - string name of the table (that is also by default the database name)
+     * @param record - the column to insert as an object of form:
+     *  const exampleRecord = {
+            name: 'Findus',
+            age: 12,
+            type: 'Cat'
+        };
+     */
+    addRow: async (tableName: string, record: object) => {
+        //TODO: check if db and table exist
+        //TODO: check matching table schema to record
+        const db = await SQLite.openDatabaseAsync(`${tableName}.db`); // open db
+
+        // convert object to strings used for query
+        const columns = Object.keys(record).join(', ');
+        const values = Object.values(record);
+        const placeholders = Object.keys(record).map(() => '?').join(', ');
+
+        const sql = `INSERT INTO ${tableName} (${columns}) VALUES (${placeholders});`;
+        //example: "INSERT INTO animals (name, age, type) VALUES (?, ?, ?)"
+        //the '?' will be replaced by the values (e.g. 'Findus', 12, 'Cat')
+        
+        //add to table with sql query
+        await db.runAsync(sql, values);
     }
-    // reading data
-    // adding data
 }
